403f3185075f6e78a6b1bc444199fc7911b2ef00
Marado-Programer <marado.pen.eng@gmail.com>
2022-03-28 10:44:10 +0100

    tiny changes

---
+++ public/views/home/home.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!defined('ROOTPATH'))
+    exit;
+
+?>
+
+<p>ola</p>
--- public/views/includes/nav.php
+++ public/views/includes/nav.php
@@ -8,20 +8,21 @@ if ($this->loginRequired && !$this->loggedIn)
 
 if ($this->loggedIn):
 ?>
-
     <p>Welcome <?=$this->userName?></p>
     <a href="<?=HOME_URI?>/login/delete/">Logout</a>
-
 <?php
-    else:
+else:
 ?>
-
     <a href="<?=HOME_URI?>/login/">Login</a>
     <ul>
+        <li><a href="<?=HOME_URI?>">Home</a></li>
+        <li><a href="<?=HOME_URI?>/user-register">User register</a></li>
+        <li><a href="<?=HOME_URI?>/noticias">Noticias</a></li>
+        <li><a href="<?=HOME_URI?>/noticias/adm">Gerir Noticias</a></li>
+        <li><a href="<?=HOME_URI?>/projetos">Projetos</a></li>
+        <li><a href="<?=HOME_URI?>/projetos/adm">Gerir Projetos</a></li>
     </ul>
-
 <?php
-    endif;
+endif;
 ?>
-
 </nav>
