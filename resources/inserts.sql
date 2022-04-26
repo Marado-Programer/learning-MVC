30b05cf8eb53f394f7920cad4c44c4e52532239b
Marado-Programer <marado.pen.eng@gmail.com>
2022-04-26 23:39:35 +0100

    first release

--- resources/inserts.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `users` VALUES
-(1,'marado','Augusto Marado','$2a$08$/q9T92IhZ6ETdhHUTwuQsOii.fmyrl80GrXFJnfjddl3h17Kbr29m','marado@marado.xyz','+351 918274344','dk3uuls9j9hbd5ov172cksn8h7','23',8.000);
-
